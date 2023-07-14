b36aa46ddc56d40e2ed26d32b0f34c4cb19ec6cf
YaoSiQian <admin@yaosiqian.cn>
2023-07-14 10:49:39 +0800

    Simply env var string

--- src/api/lib/mysql.ts
+++ src/api/lib/mysql.ts
@@ -3,14 +3,14 @@ import { Connection, createConnection } from 'mysql2/promise'
 export async function getHatDBConnection(): Promise<Connection | null> {
   try {
     const connection = await createConnection({
-      host: process.env.HATDBHOST,
+      host: process.env.DBHOST,
       port:
-        process.env.HATDBPORT !== undefined
-          ? Number.parseInt(process.env.HATDBPORT)
+        process.env.DBPORT !== undefined
+          ? Number.parseInt(process.env.DBPORT)
           : undefined,
-      user: process.env.HATDBUSER,
-      password: process.env.HATDBPASS,
-      database: process.env.HATDBNAME,
+      user: process.env.DBUSER,
+      password: process.env.DBPASS,
+      database: process.env.DBNAME,
       timezone: '+08:00',
     })
     await connection.beginTransaction()
